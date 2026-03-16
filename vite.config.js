7499d68ce75aa223b5cae7f0469c79b691bc65bc
SheepSayMeh <myth-outpost-scoop@duck.com>
2026-03-16 14:45:45 +0100

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: 'https://github.com/SheepSayMeh/Titrinator',
+    base: '/https://github.com/SheepSayMeh/Titrinator/',
 })
